b5747df423fe9956d3033a779b6c07c236ea10cb
vritant <vritoku@gmail.com>
2017-01-08 13:17:34 -0500

    CSS is hard, still working on it

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php include("partials/header.html") ?>
 <main>
   <div class="container">
-    <div id="masthead" class="display-card z-depth-3">
-      <h1>Purdue Science Student Council</h1>
+      <h1 id="masthead">PSSC</h1>
     </div>
   </div>
 </main>
