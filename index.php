ac25a0386bd86444a644fef7e209ecbf34c318d9
vritant <vritoku@gmail.com>
2017-01-08 05:07:24 -0500

    added display div

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php include("partials/header.html") ?>
 <main>
+  <div class="container">
+    <div id="masthead" class="display-card z-depth-3">
+      <h1>Purdue Science Student Council</h1>
+    </div>
+  </div>
 </main>
 <?php include("partials/footer.html") ?>
