f0710dfc6f5768b1ad0f3a3f9cd456f891e5a742
vritant24 <vritoku@gmail.com>
2017-01-14 16:49:42 -0500

    made navbar slightly transparent

--- contact.php
+++ contact.php
@@ -3,15 +3,15 @@
   <div class="container">
   <div id="contact-container" class="section white">
     <!-- <div class="container"> -->
-    <h2 class="header black-text">Contact</h2>
+    <h2 class="header black-text">Contact Us</h2>
       <div class="row">
         <form class="col s12" method="POST" action="contact_submit.php">
           <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s12 m6">
               <input id="full_name" type="text" name="fullName" class="validate">
               <label for="first_name"> Your Full Name</label>
             </div>
-              <div class="input-field col s6">
+              <div class="input-field col s12 m6">
                 <input id="email" type="email" name="email" class="validate">
                 <label for="email" data-error="please enter an email" data-success="">Your Email</label>
               </div>
@@ -22,7 +22,7 @@
               <label for="textarea1">Your Message</label>
             </div>
           </div>
-          <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+          <button class="btn waves-effect waves-light" type="submit" name="action">Send
             <i class="material-icons right">send</i>
           </button>
         </form>
