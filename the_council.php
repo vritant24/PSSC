3950b34a4b0a3289a0ecc2951f5f7da62c8be406
vritant24 <vritoku@gmail.com>
2017-01-24 08:33:10 -0500

    working on the council page

---
+++ the_council.php
@@ -0,0 +1,65 @@
+<?php include("partials/header.html") ?>
+
+<main class="white">
+  <div class="container hide-on-small-only">
+
+  <h1>The Council</h1>
+
+  In our effort to serve the College of Science here at Purdue University,
+
+  <ul class="collapsible popout" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header active grey lighten-4 teal-text">Executive Board</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header grey lighten-4 teal-text">Campus Outreach</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header grey lighten-4 teal-text">Community Outreach</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header grey lighten-4 teal-text">Publicity and Fundraising</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header grey lighten-4 teal-text">Network and Career Outreach</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+  </ul>
+
+  </div>
+
+
+  <div class="hide-on-med-and-up">
+
+  <ul class="collapsible" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header active">Executive Board</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header">Campus Outreach</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header">Community Outreach</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header">Publicity and Fundraising</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header">Network and Career Outreach</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+  </ul>
+
+  </div>
+</main>
+
+
+<?php include("partials/footer.html") ?>
