547c19fab804c17369b4db866b3a87b5fa840c33
vritant24 <vritoku@gmail.com>
2017-02-08 22:14:43 -0500

    playing with logos

--- index.php
+++ index.php
@@ -4,10 +4,17 @@
     <div id="background-output" class="background-output"></div>
 </div>
 
-<article class = "content valign-wrapper">
-<div class="title center-align">
-<h1>PSSC</h1>
-</div>
+<article class = "content valign-wrapper" style=" padding-top: 64px; display:block;">
+  <div class="center-align">
+  <p> The Outstanding Teacher Award Is Open For Voting </p>
+  <a href="outstanding-teacher-award.php" class="waves-effect waves-light btn">VOTE</a>
+  </div>
+  <!-- <div class="title center-align">
+    <h1>PSSC</h1>
+  </div> -->
+  <div class="center-align section" style="margin-top:100px; background: rgba(33, 33, 33, 0.47); border: 0;">
+    <img class="responsive-img" src="PSSC-Logo.png">
+  </div>
 </article>
 
 <script src="js/flat-surface-shader.js"></script>
